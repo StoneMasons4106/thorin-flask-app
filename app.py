b8fcd20c7daa4780b703e00c83062cf5abb8d84d
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-05-07 15:33:49 -0400

    Changed IP the app will run on.

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ def careers():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "127.0.0.1"),
+        host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
